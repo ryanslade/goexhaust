7b4308f7c19a8257580d20cc29a1e9ffc1f39bd8
Ryan Slade <ryanslade@gmail.com>
2016-08-28 14:30:57 +0200

    Use Go 1.7 testing.Run function.

--- exhaust_test.go
+++ exhaust_test.go
@@ -7,63 +7,95 @@ import (
 
 func TestGetConstantValues(t *testing.T) {
 	testCases := []struct {
+		name       string
 		code       string
 		valueCount int
 	}{
-		{code: validCode, valueCount: 3},
-		{code: invalidCode, valueCount: 3},
+		{
+			name:       "Valid Code",
+			code:       validCode,
+			valueCount: 3,
+		},
+		{
+			name:       "Invalid Code",
+			code:       invalidCode,
+			valueCount: 3,
+		},
 	}
 
 	for i, tc := range testCases {
-		c := newChecker()
-		err := c.parse(strings.NewReader(tc.code))
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = c.populateConstValues()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if len(c.constValues) != 1 {
-			t.Fatalf("Expected 1 const type, got %d (Case %d)", len(c.constValues), i)
-		}
-		for _, v := range c.constValues {
-			if len(v) != tc.valueCount {
-				t.Fatalf("Expected %d values, got %d (Case %d)", tc.valueCount, len(v), i)
+		t.Run(tc.name, func(t *testing.T) {
+			c := newChecker()
+			err := c.parse(strings.NewReader(tc.code))
+			if err != nil {
+				t.Fatal(err)
 			}
-		}
+			err = c.populateConstValues()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if len(c.constValues) != 1 {
+				t.Fatalf("Expected 1 const type, got %d (Case %d)", len(c.constValues), i)
+			}
+			for _, v := range c.constValues {
+				if len(v) != tc.valueCount {
+					t.Fatalf("Expected %d values, got %d (Case %d)", tc.valueCount, len(v), i)
+				}
+			}
+		})
 	}
 }
 
 func TestAllExhaustive(t *testing.T) {
 	testCases := []struct {
+		name       string
 		code       string
 		exhaustive bool
 	}{
-		{code: validCode, exhaustive: true},
-		{code: validCodeWithDefault, exhaustive: true},
-		{code: validCodeTwoTypes, exhaustive: true},
-		{code: validCodeWithNonCheckableSwitch, exhaustive: true},
-		{code: invalidCode, exhaustive: false},
+		{
+			name:       "Valid code",
+			code:       validCode,
+			exhaustive: true,
+		},
+		{
+			name:       "Valid code with default",
+			code:       validCodeWithDefault,
+			exhaustive: true,
+		},
+		{
+			name:       "Valid code with two types",
+			code:       validCodeTwoTypes,
+			exhaustive: true,
+		},
+		{
+			name:       "Valid code with non checkable switch",
+			code:       validCodeWithNonCheckableSwitch,
+			exhaustive: true,
+		},
+		{
+			name:       "Invalid Code",
+			code:       invalidCode,
+			exhaustive: false,
+		},
 	}
 
 	for i, tc := range testCases {
-		c := newChecker()
-		err := c.parse(strings.NewReader(tc.code))
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = c.populateConstValues()
-		if err != nil {
-			t.Fatal(err)
-		}
-		e, _ := c.allExhaustive()
-		if e != tc.exhaustive {
-			t.Errorf("Expected %v, Got %v (Case %d)", tc.exhaustive, e, i)
-			continue
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			c := newChecker()
+			err := c.parse(strings.NewReader(tc.code))
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = c.populateConstValues()
+			if err != nil {
+				t.Fatal(err)
+			}
+			e, _ := c.allExhaustive()
+			if e != tc.exhaustive {
+				t.Errorf("Expected %v, Got %v (Case %d)", tc.exhaustive, e, i)
+			}
+		})
 	}
-
 }
 
 const validCode = `
